9f89d7d61727b9d0a3025c74e8581c82f3356966
lukez999 <zluke7111@gmail.com>
2023-03-27 21:57:15 -0400

    made csv reader a top level function

--- src/utilities/StockInfo.py
+++ src/utilities/StockInfo.py
@@ -1,3 +1,8 @@
+def csv_reader(csv: str):
+    with open(csv) as f:
+        next(f)
+        reader = {ticker[0]: Ticker(ticker[0], ticker[1], ticker[2], int(ticker[3])) for ticker in f}
+        return reader
 class Ticker:
     symbol: str
     name: str
@@ -11,10 +16,12 @@ class Ticker:
 
 
 class StockInfo:
-    tickers: dict[str, Ticker]
+    _tickers: dict[str, Ticker]
     def __init__(self, csv: str):
-        with open(csv) as f:
-            next(f)
-            reader = {ticker[0]: Ticker(ticker[0], ticker[1], ticker[2], int(ticker[3])) for ticker in f}
-            self.tickers = reader
-
+        self._tickers = csv_reader(csv)
+    def get_ticker_name(self, symbol: str):
+        return self._tickers[symbol].name
+    def get_ticker_industry(self, symbol: str):
+        return self._tickers[symbol].industry
+    def get_ticker_market_cap(self, symbol: str):
+        return self._tickers[symbol].market_cap
