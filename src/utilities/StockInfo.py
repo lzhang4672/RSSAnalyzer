313bb3e864fce6bd3d9ce03bd8764c7499051b40
lukez999 <zluke7111@gmail.com>
2023-03-28 23:08:12 -0400

    .

--- src/utilities/StockInfo.py
+++ src/utilities/StockInfo.py
@@ -4,13 +4,6 @@ from CSV import csv_reader
 
 tickers = csv_reader('tickers_data.csv')
 
-def get_name(ticker):
-    return tickers[ticker]['name']
 
-def get_industry(ticker):
-    return tickers[ticker]['industry']
-
-def get_market_cap(ticker):
-    return tickers[ticker]['market cap']
 
 
