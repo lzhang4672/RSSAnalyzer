8f1f9616d219e392db5cb10da0610f9bd9f4a0a6
MarkZhang3 <72476129+MarkZhang3@users.noreply.github.com>
2023-03-29 11:03:26 -0400

    update stockInfo.py

--- src/utilities/StockInfo.py
+++ src/utilities/StockInfo.py
@@ -6,12 +6,13 @@ from typing import Optional
 tickers = read_file('../data/tickers_data.csv')
 
 
-def get_info_from_ticker(ticker: str, csv_dict: list[dict]) -> Optional[dict]:
+def get_info_from_ticker(ticker: str) -> dict | None:
     """
     Returns the dictionary inside the given list that corresponds to the ticker
     If the symbol is not found, returns None
     """
-    for stock in csv_dict:
+
+    for stock in tickers:
         if ticker in stock:
             return stock
 
