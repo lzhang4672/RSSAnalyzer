913516a380da0745be12785b48858fe81fe94229
lukez999 <zluke7111@gmail.com>
2023-03-28 20:17:48 -0400

    created class for csv functions

--- src/utilities/StockInfo.py
+++ src/utilities/StockInfo.py
@@ -1,3 +1,4 @@
+from python_ta.contracts import check_contracts
 @check_contracts
 class Ticker:
     """A Ticker represents a stock in a network.
@@ -10,7 +11,7 @@ class Ticker:
     name: str
     industry: str
     market_cap: int
-    def __init__(self, symbol: str, name: str, industry: str, market_cap: int):
+    def __init__(self, symbol: str, name: str, industry: str, market_cap: int) -> None:
         self.symbol = symbol
         self.name = name
         self.industry = industry
@@ -22,18 +23,13 @@ class StockInfo:
     Instance Attributes:
     - _tickers: a dictionary with ticker symbols as keys and corresponding Ticker as values """
     _tickers: dict[str, Ticker]
-    def __init__(self, csv: str):
+    def __init__(self, csv: str) -> None:
         self._tickers = csv_reader(csv)
-    def get_ticker_name(self, symbol: str):
+    def get_ticker_name(self, symbol: str) -> str:
         return self._tickers[symbol].name
-    def get_ticker_industry(self, symbol: str):
+    def get_ticker_industry(self, symbol: str) -> str:
         return self._tickers[symbol].industry
-    def get_ticker_market_cap(self, symbol: str):
+    def get_ticker_market_cap(self, symbol: str) -> int:
         return self._tickers[symbol].market_cap
 
-def csv_reader(csv: str) -> dict[str, Ticker]:
-    """Return a mapping of ticker symbols to the Ticker object"""
-    with open(csv) as f:
-        next(f)
-        reader = {ticker[0]: Ticker(ticker[0], ticker[1], ticker[2], int(ticker[3])) for ticker in f}
-        return reader
+
