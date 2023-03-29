a334d36d718f3de5a5453e58382e9df27379d57c
MarkZhang3 <72476129+MarkZhang3@users.noreply.github.com>
2023-03-29 11:15:39 -0400

    update stockInfo.py

--- src/utilities/StockInfo.py
+++ src/utilities/StockInfo.py
@@ -6,24 +6,32 @@ from typing import Optional
 tickers = read_file('../data/tickers_data.csv')
 
 
-def get_info_from_ticker(ticker: str) -> dict | None:
+def get_info_from_ticker(ticker: str) -> dict[str, str] | None:
     """
     Returns the dictionary inside the given list that corresponds to the ticker
     If the symbol is not found, returns None
     """
 
     for stock in tickers:
-        if ticker.upper() in stock.values():
+        if ticker.upper() == stock['Symbol'].upper():
             return stock
 
     return None
 
 
 def get_ticker_from_name(name: str) -> str | None:
-
-
+    """
+    Returns the corresponding dictionary to the name specified
+    Otherwise returns None if not found
+    """
+    for stock in tickers:
+        if name.lower() == stock['Name'].lower():
+            return stock
+    return None
 
 
 def get_tickers() -> list[str]:
-
-
+    """
+    Returns a list containing all the tickers in the csv
+    """
+    return [stock['Symbol'] for stock in tickers]
