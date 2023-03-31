74d71b68341aea689c135f0944c39491465d6cf4
Li Zhang <lijianzhang2@gmail.com>
2023-03-31 18:55:31 -0400

    added csv file and updated NewsScraper and StockAnalyzer.py

--- src/AnalysisRunner.py
+++ src/AnalysisRunner.py
@@ -5,6 +5,8 @@ from StockInfo import get_tickers
 from StockAnalyzer import StockAnalyzer, StockAnalyzerSettings
 
 
+print('test')
+
 # CONSTANTS
 default_settings = StockAnalyzerSettings(id='all_tickers', articles_per_ticker=20, use_cache=True,
                                          search_focus='Competitors')
