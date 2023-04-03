659215d45ba0bb73c55a179c8fcaf5910a216395
lukez999 <zluke7111@gmail.com>
2023-04-03 09:09:52 -0400

    added docstrings

--- src/GUI.py
+++ src/GUI.py
@@ -1,5 +1,18 @@
 from tkinter import *
+"""
+This python module contains SearchBar, DisplayList, ScrapeLive, LoadPreset and Main to display a GUI
 
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of TAs and professors
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2023 Mark Zhang, Li Zhang and Luke Zhang
+"""
 
 class SearchBar():
     """
